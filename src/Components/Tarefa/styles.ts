d5bac1e0fbacb6c4e680dd75761747b1f6e58d42
Guilherme Augusto <gui.gutinks@gmail.com>
2025-06-24 15:57:54 -0400

    making the whole visual part of the project

---
+++ src/Components/Tarefa/styles.ts
@@ -0,0 +1,95 @@
+import styled from 'styled-components'
+import variaveis from '../../styles/variaveis'
+
+import * as enums from '../../utils/enums/tarefa'
+
+type TagProps = {
+  prioridade?: enums.Prioridade
+  status?: enums.Status
+  parametro: 'status' | 'prioridade'
+}
+
+function retornaCorDeFundo(props: TagProps) {
+  if (props.parametro === 'status') {
+    switch (props.status) {
+      case enums.Status.PENDENTE:
+        return variaveis.amarelo
+      case enums.Status.CONCLUIDA:
+        return variaveis.verde
+      default:
+        return '#ccc'
+    }
+  } else if (props.parametro === 'prioridade') {
+    switch (props.prioridade) {
+      case enums.Prioridade.URGENTE:
+        return variaveis.vermelho
+      case enums.Prioridade.IMPORTANTE:
+        return variaveis.amarelo2
+      case enums.Prioridade.NORMAL:
+        return variaveis.verde
+      default:
+        return '#ccc'
+    }
+  }
+}
+
+export const Card = styled.div`
+  background-color: #fcfcfc;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  padding: 16px;
+  border-radius: 8px;
+  margin-bottom: 32px;
+`
+
+export const Titulo = styled.h3`
+  font-size: 18px;
+  font-weight: bold;
+`
+
+export const Tag = styled.span<TagProps>`
+  color: #fff;
+  padding: 4px 8px;
+  font-size: 10px;
+  font-weight: bold;
+  background-color: ${(props) => retornaCorDeFundo(props)};
+  margin-right: 8px;
+  border-radius: 8px;
+  display: inline-block;
+`
+
+export const Descricao = styled.textarea`
+  color: #8b8b8b;
+  font-size: 14px;
+  line-height: 24px;
+  font-family: Roboto Mono, monospace;
+  display: block;
+  width: 100%;
+  margin-bottom: 16px;
+  resize: none;
+  border: none;
+  background-color: transparent;
+`
+
+export const BarraAcoes = styled.div`
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  padding-top: 16px;
+`
+
+export const Botao = styled.button`
+  color: #fff;
+  background-color: #2f3640;
+  font-weight: bold;
+  font-size: 12px;
+  padding: 8px 12px;
+  margin-right: 8px;
+  cursor: pointer;
+  border-radius: 8px;
+  border: none;
+`
+export const BotaoSalvar = styled(Botao)`
+  background-color: ${variaveis.verde};
+`
+
+export const BotaoCancelarRemover = styled(Botao)`
+  background-color: ${variaveis.vermelho};
+`
--- src/Containers/ListaDeTarefas/index.tsx
+++ src/Containers/ListaDeTarefas/index.tsx
@@ -1,10 +1,51 @@
+import Tarefa from '../../Components/Tarefa'
+import * as S from './styles'
+
+import * as enums from '../../utils/enums/tarefa'
+
+const tarefas = [
+  {
+    titulo: 'Estudar Typescript',
+    descricao: 'assistir o modulo correspondente na EBAC',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  },
+  {
+    titulo: 'pagar conta de internet',
+    descricao: 'baixar fatura no email',
+    prioridade: enums.Prioridade.URGENTE,
+    status: enums.Status.CONCLUIDA
+  },
+  {
+    titulo: 'ir pra academia',
+    descricao: 'fazer treino de braco',
+    prioridade: enums.Prioridade.NORMAL,
+    status: enums.Status.PENDENTE
+  }
+]
+
 const ListaDeTarefas = () => {
   return (
-    <main>
+    <S.Container>
       <p>
-        2 Tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;
+        {2} tarefas marcadas como &quot; categoria &ldquo; e &quot; termo
+        &ldquo;
       </p>
-    </main>
+      <S.Lista>
+        <li>
+          {tarefas.map((t) => (
+            <li key={t.titulo}>
+              <Tarefa
+                titulo={t.titulo}
+                prioridade={t.prioridade}
+                status={t.status}
+                descricao={t.descricao}
+              />
+            </li>
+          ))}
+        </li>
+      </S.Lista>
+    </S.Container>
   )
 }
 
